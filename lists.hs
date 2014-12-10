36e95028ae2724cba6b72251c5c97e09528ed8b1
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2014-12-10 00:20:07 -0800

    Updated lists.hs

--- lists.hs
+++ lists.hs
@@ -1,3 +1,11 @@
 -- Take the first element from a list.
-getFirstElementFromList l =
-	take first 1 from [1 2 3]
+-- Option 1:
+getFirstElementFromList :: [a] -> [a]
+getFirstElementFromList l = take 1 l
+
+-- Option 2 (Wrote this after taking class on programming languages
+-- which taught some formal Haskell:
+getFirstElementFromList2 :: [a] -> a 
+getFirstElementFromList2 (x:xs) = x
+
+
