46408e2e9b2aa23b816a4c5191d4a2ea3e106581
Azadi Bogolubov <AzadiBogolubov@gmail.com>
2014-12-10 00:20:55 -0800

    Fixed typo in lists.hs

--- lists.hs
+++ lists.hs
@@ -3,8 +3,7 @@
 getFirstElementFromList :: [a] -> [a]
 getFirstElementFromList l = take 1 l
 
--- Option 2 (Wrote this after taking class on programming languages
--- which taught some formal Haskell:
+-- Option 2 (Wrote this after taking class on programming languages which taught some formal Haskell):
 getFirstElementFromList2 :: [a] -> a 
 getFirstElementFromList2 (x:xs) = x
 
